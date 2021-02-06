76ff8876c74d6e69077f3db5d2a23dc8b52e5081
austinfelker <felkeraustin@gmail.com>
2021-02-05 23:25:18 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>lahsdfauishdfiuhsdiflahsfiwheufh</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p class="c1">Spy Monkey</p>
+                    <p>Email: aowdji@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Red Monkey</p>
+                    <p>Email: aowdji@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Wise Monkey</p>
+                    <p>Email: aowdji@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Baboon</p>
+                    <p>Email: aowdji@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
