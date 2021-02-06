8287f0917d3fc10cb9dfb527ac2bf2aa4d2a6220
austinfelker <felkeraustin@gmail.com>
2021-02-06 03:52:36 -0500

    changed title

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <body>
+   <title>MonkeyCritic.com</title>
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
@@ -10,9 +11,9 @@
             <div class="card">
                 <img src="images/1.jpeg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p class="c1">Spy Monkey</p>
-                    <p>Email: aowdji@cs230.com</p>
+                    <h2>Spy Monkey</h2>
+                    <p class="c1">CEO</p>
+                    <p>This is not a real monkey at all, but rather humans pretending to be a monkey. This leads to inconsistent reviews when compared to other review members. This actually the creator of the website and aims to find out more about monkey behavior.</p>
                 </div>
             </div>
         </div>
@@ -20,9 +21,8 @@
             <div class="card">
                 <img src="images/2.jpeg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Red Monkey</p>
-                    <p>Email: aowdji@cs230.com</p>
+                    <h2>Red Monkey</h2>
+                    <p>This is your average reviewer. He likes a good action or drama series or movie. He is most in touch with the average population.</p>
                 </div>
             </div>
         </div>
@@ -30,9 +30,8 @@
             <div class="card">
                 <img src="images/3.jpeg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Wise Monkey</p>
-                    <p>Email: aowdji@cs230.com</p>
+                    <h2>Wise Monkey</h2>
+                    <p>The most formal and in-depth reviewer on the website. His long life has given him the experience and grasp on movies that allows him to fully critique films. He is not easily amused.</p>
                 </div>
             </div>
         </div>
@@ -40,9 +39,8 @@
             <div class="card">
                 <img src="images/4.jpeg">
                 <div class="container">
-                    <h2>CEO</h2>
-                    <p>Baboon</p>
-                    <p>Email: aowdji@cs230.com</p>
+                    <h2>Baboon</h2>
+                    <p>This monkey has not yet mastered the use of the keyboard, or the English language for that matter. He is said to have more mental stimulation when watching war movies; oftentimes this sets him off on a frezy rage in which he spams the keyboard breaks it.</p>
                 </div>
             </div>
         </div>
