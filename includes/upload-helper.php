312c57f2028e843f1e8f4362f1d013a26ec1bf9d
austinfelker <felkeraustin@gmail.com>
2021-02-28 00:19:30 -0500

    Lab5v2

--- includes/upload-helper.php
+++ includes/upload-helper.php
@@ -7,7 +7,6 @@ define('KB', 1024); //variable used to designat the file size
 define('MB', 1048576);
 
 if (isset($_POST['prof-submit'])) { //taken from profile.php
-    echo "awoidjaid";
 
     $uname = $_SESSION['uname']; //taken from the login helper
     $file = $_FILES['prof-image']; //taken from the profile.php. anytime we use the global variabel with [] we are typing in 'theNameOfTheVariable'
@@ -21,31 +20,30 @@ if (isset($_POST['prof-submit'])) { //taken from profile.php
     $allowed = array('jpg', 'jpeg', 'png', 'svg');
 
     if ($file_error !== 0) { //if there is a file error
-        header("Location ../profile.php?error=UploadError");
+        header("Location: ../profile.php?error=UploadError");
         exit();
     }
     if (!in_array($ext, $allowed)) { //if not in array
-        header("Location ../profile.php?error=InvalidType");
+        header("Location: ../profile.php?error=InvalidType");
         exit();
     }
     if ($file_size > 4*MB) { //if file size > 4 MB
-        header("Location ../profile.php?error=FileSizeExceeded");
+        header("Location: ../profile.php?error=FileSizeExceeded");
         exit();
     }
 
         $new_name = uniqid('',true).".".$ext; //creates the name of the profile pic and appends 10 random chars at the end of it to add uniqueness
-        $destination = '..profiles/'.$new_name; //variable for the location of the prof pic to the profiles folder
+        $destination = '../profiles/'.$new_name; //variable for the location of the prof pic to the profiles folder
         $sql = "UPDATE profiles SET profpic='$destination' WHERE uname='$uname'"; //updates the profile in mysql
         mysqli_query($conn, $sql);
         move_uploaded_file($file_tmp_name, $destination);
 
-        header("Location ../profile.php?success=UploadWin");
+        header("Location: ../profile.php?success=UploadWin");
         exit();
 
 }
 else {
-    echo "FAIL";
-    header("Location ../profile.php");
+    header("Location: ../profile.php");
     exit();
 
 }
